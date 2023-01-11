ed657f853be3b506e499ac9388e05b2faa7fd218
patsicko <patsicko@g>
2023-01-11 21:48:00 +0200

    ft(Contact):Retrieve contact message -Create API endpoint for retrieving all contact messages -Create API endpoint for retrieving single message [Finishes#]

--- controller/ContactController.js
+++ controller/ContactController.js
@@ -18,6 +18,42 @@ class ContactController {
       }
 
 
+
+    static async getMessages(req,res) {
+      try {
+     const messages=await Message.find();
+     res.status(200).json(messages)
+      }catch (error){
+          res.status(404).json(error.message);
+      }
+  }
+
+
+  static async getSingleMessage(req,res) {
+    try {
+   const message=await Message.findById(req.param.id);
+   res.status(200).json(message)
+    }catch (error){
+        res.status(404).json(error.message);
+    }
+}
+
+
+  static async deleteMessage(req,res){
+    try{
+     
+      await Message.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+          message:"message deleted"
+        })
+      
+      
+    }catch(error){
+      res.status(404).json(error.message)
+    }
+  }
+
+
 }
 
 
--- routes/ContactRoute.js
+++ routes/ContactRoute.js
@@ -4,6 +4,8 @@ import ContactController from "../controller/ContactController";
 const ContactRoute=express.Router();
 
 ContactRoute.post("/createMessage",ContactController.createMessage);
-
+ContactRoute.get("/getMessages/",ContactController.getMessages);
+ContactRoute.get("/getSingleMessage/:id",ContactController.getSingleMessage);
+ContactRoute.get("/deleteMessage/:id",ContactController.deleteMessage);
 
  export default ContactRoute
