5fff422dc4102eb2a97572cc5662e7c5e058dd1d
patsicko <patsicko@g>
2023-01-25 19:13:58 +0200

    fix(Users) Edit user accessibility [Finishes#]

--- src/routes/apis/blogRoute.js
+++ src/routes/apis/blogRoute.js
@@ -8,8 +8,8 @@ import {isAdmin,findUserById} from "../../middleware/isAdmin";
 const BlogRoute=Express.Router();
 
 BlogRoute.post("/createBlog/:id",verifyToken,findUserById,isAdmin,upload.single('blogImage'),validateBlog,BlogController.createBlog);
-BlogRoute.get("/getBlogs",verifyToken,verifyToken,BlogController.retrieveBlogAll);
-BlogRoute.get("/getBlog/:id",verifyToken,BlogController.retrieveBlogSingle);
+BlogRoute.get("/getBlogs",BlogController.retrieveBlogAll);
+BlogRoute.get("/getBlog/:id",BlogController.retrieveBlogSingle);
 BlogRoute.get("/deleteBlog/:id",verifyToken,BlogController.deleteBlog);
 BlogRoute.put("/updateBlog/:id",upload.single('blogImage'),validateBlog,BlogController.updateBlog);
 
